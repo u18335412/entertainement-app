e6f62ede636da24561a4228c6576443aaeec5435
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-22 22:09:55 +0200

    📄:login page with basic validation

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import InputField from "../src/components/shared/input";
+import { useForm, FormProvider } from "react-hook-form";
+
 const Login = () => {
+  const methods = useForm();
+  const handleSubmit = (data) => {
+    console.table(data);
+  };
   return (
-    <main className="flex justify-center">
-      <p>Login</p>
+    <main className="flex justify-center flex-col items-center py-[4.875rem]">
+      <img src="/favicon.svg" className="w-[2rem] h-[1.6rem]" alt="logo" />
+      <div className="mt-[5.183rem] w-[25rem] p-8 bg-Semi-Dark-Blue text-Pure-White rounded-[20px] flex-col">
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(handleSubmit)}>
+            <p className=" text-lg font-light">Login</p>
+            <div className=" mt-[2.5rem] space-y-[1.5rem]">
+              <InputField text={"Email Address"} name="email" />
+              <InputField text={"Password"} name="password" type="password" />
+            </div>
+            <div className="mt-[2.5rem] text-center text-sm font-light">
+              <button
+                type="submit"
+                className="bg-Red py-[0.95rem] hover:bg-Pure-White transition-all hover:text-Dark-Blue w-full rounded-md font-light "
+              >
+                Login to you account
+              </button>
+              <p className=" mt-[1.5rem] flex gap-x-[9px] justify-center">
+                Dont have an account?
+                <Link href="/signup" passHref>
+                  <span className="text-Red cursor-pointer hover:opacity-80">
+                    Sign Up
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </form>
+        </FormProvider>
+      </div>
     </main>
   );
 };
