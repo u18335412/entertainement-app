f758e3b68d946af31e00a6a853044735ed2650ab
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-23 14:36:27 +0200

    input refactor

--- src/components/shared/input.js
+++ src/components/shared/input.js
@@ -1,4 +1,4 @@
-import { useFormContext} from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
 const InputField = ({ text, name, type = "text", required = true }) => {
   const {
@@ -13,13 +13,15 @@ const InputField = ({ text, name, type = "text", required = true }) => {
         placeholder={text}
         name={name}
         {...register(name, { required: required })}
-        className={`px-4 pb-[1.125rem] focus:outline-none focus:border-b-Pure-White bg-transparent caret-Red transition-all border-b w-full placeholder:text-sm  
+        className={`px-4 pb-[1.125rem] focus:outline-none focus:border-b-Pure-White text-white bg-transparent caret-Red transition-all border-b w-full placeholder:text-sm autofill:bg-rose-400 
         ${errors[`${name}`] ? "border-Red" : "border-Greyish-Blue"}`}
       />
-      {errors[`${name}`] && (
-        <p className="text-Red text-xs font-light top-0 absolute right-0">
+      {errors[`${name}`]?.type === "required" ? (
+        <p className="absolute top-0 right-0 text-xs font-light text-Red">
           {"Can't be empty"}
         </p>
+      ) : (
+        ""
       )}
     </div>
   );
