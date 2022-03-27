751b1b3a7366fe4ac910fdc455bdc1547556322c
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-27 18:27:56 +0200

    refactor search componetn

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 const SearchComponent = () => {
   return (
-    <div className="flex px-1 w-fit gap-x-[1.5rem] items-center ">
+    <div className="flex mt-16 px-1 w-fit gap-x-[1.5rem] items-center ">
       <img
         src="/assets/icon-search.svg"
-        className="object-cover w-[1.5rem] h-[1.5rem]"
+        className="object-cover pb-[0.875em] w-[1.5rem] min-h-[1.5rem]"
         alt="search icon"
       />
       <input
         type="text"
-        className=" bg-transparent font-light text-[1.5rem] border-b caret-Red transition-opacity focus:border-opacity-100 border-opacity-0 border-Greyish-Blue  focus:outline-none w-[34.375rem] py-[0.9rem]"
+        className=" bg-transparent font-light text-[1.5rem] border-b caret-Red transition-opacity focus:border-opacity-100 border-opacity-0 border-Greyish-Blue  focus:outline-none pb-[0.875em] w-[34.375rem] "
         placeholder="Search for movies or TV series"
       />
     </div>
