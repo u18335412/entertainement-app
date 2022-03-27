32240e145d0b9ea37d63f9b7bf2a7fb470949d1e
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-27 18:26:51 +0200

    📄: movies page

---
+++ pages/movies.js
@@ -0,0 +1,12 @@
+import SearchComponent from "../src/components/search";
+
+const Movies = () => {
+  return (
+    <div className="text-white">
+      <SearchComponent></SearchComponent>
+      Movies
+    </div>
+  );
+};
+
+export default Movies;
