9c13f8febd9751d35fcf398eb55e1e3e6a72c11b
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-27 18:26:34 +0200

    📄:tv-series page

---
+++ pages/tv-series.js
@@ -0,0 +1,12 @@
+import SearchComponent from "../src/components/search";
+
+const TvSeries = () => {
+  return (
+    <div className="text-white">
+      <SearchComponent></SearchComponent>
+      TV Series
+    </div>
+  );
+};
+
+export default TvSeries;
