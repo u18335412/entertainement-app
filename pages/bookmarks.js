0475f05e5ba387c3a99441eca45d37149a7712e2
u18335412@tuks.co.za <u18335412@tuks.co.za>
2022-03-27 18:26:13 +0200

    📄: bookmark page

---
+++ pages/bookmarks.js
@@ -0,0 +1,12 @@
+import SearchComponent from "../src/components/search";
+
+const Bookmarks = () => {
+  return (
+    <div className="text-white">
+      <SearchComponent></SearchComponent>
+     Bookmarks
+    </div>
+  );
+};
+
+export default Bookmarks;
